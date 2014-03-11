65334636a918387b91ea622778dbfb773b216929
Florian Schweikert <kelvan@ist-total.org>
2014-03-11 22:50:29 +0100

    use line_count as weight for station search
    update pyql submodule

--- glue/gui_search.py
+++ glue/gui_search.py
@@ -2,7 +2,7 @@ from pyWL.database import Station
 
 class Stops:
     def get(self, term):
-        stations = Station.search_by_name(term)
+        stations = Station.search_by_name(term, weight='line_count')
         return [x['name'] for x in stations]
 
 stops = Stops()
