bdc124b61d60151804205cebb1a014cf0052eb68
Florian Schweikert <kelvan@ist-total.org>
2014-03-11 21:58:49 +0100

    use exact search for station
    update submodule pywl

--- glue/gui_departures.py
+++ glue/gui_departures.py
@@ -6,7 +6,7 @@ from database import Station
 
 class Deps:
     def get(self, name):
-        stations = Station.search_by_name(name)
+        stations = Station.search_by_name(name, exact=True)
 
         rbl = []
         for st in stations:
@@ -17,11 +17,6 @@ class Deps:
         result_dicts = []
         for station in sorted(departures.values(), key=itemgetter('name')):
             for departure in sorted(station['departures'], key=itemgetter('countdown')):
-                bar = ('timePlanned', 'timeReal')
-                for foo in bar:
-                    if foo in departure:
-                        del(departure[foo])
-
                 result_dicts.append(departure)
 
         return result_dicts
