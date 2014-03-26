4821c07102c9570bd98e90512fcdecf102ed5c5d
Christian Ratzenhofer <christian.ratzenhofer@cdnm.at>
2014-03-26 15:01:59 +0100

    silica: lupdate main translation file

--- translations/silica/sailtoVienna.ts
+++ translations/silica/sailtoVienna.ts
@@ -4,23 +4,28 @@
 <context>
     <name></name>
     <message id="settings">
+        <location filename="../qml/StartPage.qml" line="17"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="nearby">
+        <location filename="../qml/StartPage.qml" line="22"/>
         <source>Nearby</source>
         <translation type="unfinished"></translation>
     </message>
     <message id="clearrecent">
+        <location filename="../qml/StartPage.qml" line="32"/>
         <source>Clear recent</source>
         <translation type="unfinished"></translation>
     </message>
-    <message id="recent">
-        <source>Recent</source>
+    <message id="bookmarks">
+        <location filename="../qml/StartPage.qml" line="40"/>
+        <source>Bookmarks</source>
         <translation type="unfinished"></translation>
     </message>
-    <message id="bookmarks">
-        <source>Bookmarks</source>
+    <message id="recent">
+        <location filename="../qml/StartPage.qml" line="46"/>
+        <source>Recent</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
