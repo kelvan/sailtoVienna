322f1a6106cd7aacc303718f74a373ccf9196b3d
Filip Kłębczyk <fklebczyk@gmail.com>
2014-04-19 17:09:14 +0200

    Added Polish translation

---
+++ translations/silica/harbour-sailtovienna-pl.ts
@@ -0,0 +1,145 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name></name>
+    <message id="settings">
+        <location filename="../qml/NearBy.qml" line="24"/>
+        <location filename="../qml/Search.qml" line="22"/>
+        <location filename="../qml/Settings.qml" line="12"/>
+        <location filename="../qml/StartPage.qml" line="17"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message id="search">
+        <location filename="../qml/NearBy.qml" line="29"/>
+        <location filename="../qml/Search.qml" line="36"/>
+        <location filename="../qml/Settings.qml" line="52"/>
+        <source>Search</source>
+        <translation>Szukaj</translation>
+    </message>
+    <message id="nearby">
+        <location filename="../qml/NearBy.qml" line="37"/>
+        <location filename="../qml/Search.qml" line="28"/>
+        <location filename="../qml/Settings.qml" line="54"/>
+        <location filename="../qml/StartPage.qml" line="22"/>
+        <source>Nearby</source>
+        <translation>Pobliskie</translation>
+    </message>
+    <message id="looking-for-stations">
+        <location filename="../qml/NearBy.qml" line="52"/>
+        <source>Looking for stations</source>
+        <translation>Szukam stacji</translation>
+    </message>
+    <message id="km">
+        <location filename="../qml/NearBy.qml" line="72"/>
+        <source>km</source>
+        <translation>km</translation>
+    </message>
+    <message id="add-to-favourites">
+        <location filename="../qml/NearBy.qml" line="91"/>
+        <location filename="../qml/Result.qml" line="37"/>
+        <location filename="../qml/Search.qml" line="92"/>
+        <source>Add to favourites</source>
+        <translation>Dodaj do ulubionych</translation>
+    </message>
+    <message id="clearrecent">
+        <location filename="../qml/StartPage.qml" line="32"/>
+        <source>Clear recent</source>
+        <translation>Usuń ostatnie</translation>
+    </message>
+    <message id="bookmarks">
+        <location filename="../qml/StartPage.qml" line="40"/>
+        <source>Bookmarks</source>
+        <translation>Ulubione</translation>
+    </message>
+    <message id="recent">
+        <location filename="../qml/StartPage.qml" line="68"/>
+        <source>Recent</source>
+        <translation>Ostatnie</translation>
+    </message>
+    <message id="search-for">
+        <location filename="../qml/Settings.qml" line="18"/>
+        <source>Search for</source>
+        <translation>Szukaj dla</translation>
+    </message>
+    <message id="station">
+        <location filename="../qml/Search.qml" line="43"/>
+        <location filename="../qml/Search.qml" line="44"/>
+        <location filename="../qml/Settings.qml" line="22"/>
+        <source>Station</source>
+        <translation>Stacja</translation>
+    </message>
+    <message id="search-stations">
+        <location filename="../qml/Search.qml" line="63"/>
+        <source>Search stations</source>
+        <translation>Szukaj stacje</translation>
+    </message>
+    <message id="line">
+        <location filename="../qml/Settings.qml" line="24"/>
+        <source>Line</source>
+        <translation>Linia</translation>
+    </message>
+    <message id="automatic-refresh">
+        <location filename="../qml/Settings.qml" line="30"/>
+        <source>Automatic refresh</source>
+        <translation>Automatyczne odświeżanie</translation>
+    </message>
+    <message id="off">
+        <location filename="../qml/Settings.qml" line="34"/>
+        <source>Off</source>
+        <translation>Wyłącz</translation>
+    </message>
+    <message id="15-sec">
+        <location filename="../qml/Settings.qml" line="36"/>
+        <source>15 sec</source>
+        <translation>15 sekund</translation>
+    </message>
+    <message id="30-sec">
+        <location filename="../qml/Settings.qml" line="38"/>
+        <source>30 sec</source>
+        <translation>30 sekund</translation>
+    </message>
+    <message id="1-min">
+        <location filename="../qml/Settings.qml" line="40"/>
+        <source>1 min</source>
+        <translation>1 minuta</translation>
+    </message>
+    <message id="2-min">
+        <location filename="../qml/Settings.qml" line="42"/>
+        <source>2 min</source>
+        <translation>2 minuty</translation>
+    </message>
+    <message id="result-view-cover-action">
+        <location filename="../qml/Settings.qml" line="48"/>
+        <source>Result view cover action</source>
+        <translation>Akcja okładki widok wyników</translation>
+    </message>
+    <message id="refreshing...">
+        <location filename="../qml/CoverContainer.qml" line="22"/>
+        <location filename="../qml/Result.qml" line="53"/>
+        <source>Refreshing...</source>
+        <translation>Odświeżanie...</translation>
+    </message>
+    <message id="remove-from-favourites">
+        <location filename="../qml/Result.qml" line="35"/>
+        <source>Remove from favourites</source>
+        <translation>Usuń z ulubionych</translation>
+    </message>
+    <message id="refresh">
+        <location filename="../qml/Result.qml" line="55"/>
+        <source>Refresh</source>
+        <translation>Odśwież</translation>
+    </message>
+    <message id="result">
+        <location filename="../qml/Result.qml" line="66"/>
+        <source>Result</source>
+        <translation>Wynik</translation>
+    </message>
+    <message id="no-departures-found">
+        <location filename="../qml/Result.qml" line="102"/>
+        <source>No departures found</source>
+        <translation>Nie znaleziono żadnych odjazdów</translation>
+    </message>
+</context>
+</TS>
